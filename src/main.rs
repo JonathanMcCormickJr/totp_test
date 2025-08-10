73dd6f980c839c23ba575c88c55ea4e73a9ec8a8
Jonathan McCormick <67705789+JonathanMcCormickJr@users.noreply.github.com>
2025-08-09 21:55:15 -0500

    details update

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ fn main() {
         1,
         30,
         Secret::Raw("TestSecretSuperSecret".as_bytes().to_vec()).to_bytes().unwrap(),
-        Some("Github".to_string()),
-        "constantoine@github.com".to_string(),
+        Some("Jonathan".to_string()),
+        "user-email@domain.com".to_string(),
     ).unwrap();
     //let qr_code = totp.get_qr_base64().unwrap();
     //println!("{}", qr_code);   
